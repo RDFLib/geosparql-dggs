c24785aad9b96ea464890c80dd324157b56eea11
david-habgood <david.habgood@surroundaustralia.com>
2021-09-20 12:58:19 +1000

    Update tests from unittest -> pytest

--- tests/test_geosparql.py
+++ tests/test_geosparql.py
@@ -1,4 +1,3 @@
-import unittest
 from rdflib import Literal, Graph, Namespace, URIRef
 from gsdggs import DGGS
 
@@ -19,149 +18,144 @@ g.add((geom_d, GEO.hasGeometry, Literal("CELLLIST ((R40 R41 R43 R44))")))
 g.add((geom_g, GEO.hasGeometry, Literal("CELLLIST ((R40 R41 R43 R44))")))
 
 
-class SfRelationships(unittest.TestCase):
-    def test_contains(self):
-        # A contains B
-        result = g.query(
-            """PREFIX geo: <http://www.opengis.net/ont/geosparql#>
-        PREFIX dggs: <https://placeholder.com/dggsfuncs/>
-        SELECT ?a ?b {?a geo:hasGeometry ?a_geom .
-                    ?b geo:hasGeometry ?b_geom .
-                    FILTER dggs:sfContains(?a_geom, ?b_geom) }"""
-        )
-        result = [{str(k): v for k, v in i.items()} for i in result.bindings]
-        assert len(result) == 1
-        self.assertTrue(result[0]["a"] == geom_a and result[0]["b"] == geom_b)
+def test_contains():
+    # A contains B
+    result = g.query(
+        """PREFIX geo: <http://www.opengis.net/ont/geosparql#>
+    PREFIX dggs: <https://placeholder.com/dggsfuncs/>
+    SELECT ?a ?b {?a geo:hasGeometry ?a_geom .
+                ?b geo:hasGeometry ?b_geom .
+                FILTER dggs:sfContains(?a_geom, ?b_geom) }"""
+    )
+    result = [{str(k): v for k, v in i.items()} for i in result.bindings]
+    assert len(result) == 1
+    assert result[0]["a"] == geom_a and result[0]["b"] == geom_b
 
-    def test_within(self):
-        # B is within A
-        result = g.query(
-            """PREFIX geo: <http://www.opengis.net/ont/geosparql#>
-        PREFIX dggs: <https://placeholder.com/dggsfuncs/>
-        SELECT ?a ?b {?a geo:hasGeometry ?a_geom .
-                    ?b geo:hasGeometry ?b_geom .
-                    FILTER dggs:sfWithin(?a_geom, ?b_geom) }"""
-        )
-        result = [{str(k): v for k, v in i.items()} for i in result.bindings]
-        assert len(result) == 1
-        self.assertTrue(result[0]["a"] == geom_b and result[0]["b"] == geom_a)
+def test_within():
+    # B is within A
+    result = g.query(
+        """PREFIX geo: <http://www.opengis.net/ont/geosparql#>
+    PREFIX dggs: <https://placeholder.com/dggsfuncs/>
+    SELECT ?a ?b {?a geo:hasGeometry ?a_geom .
+                ?b geo:hasGeometry ?b_geom .
+                FILTER dggs:sfWithin(?a_geom, ?b_geom) }"""
+    )
+    result = [{str(k): v for k, v in i.items()} for i in result.bindings]
+    assert len(result) == 1
+    assert result[0]["a"] == geom_b and result[0]["b"] == geom_a
 
-    def test_intersects(self):
-        # A intersects B
-        # A intersects C
-        # A intersects D
-        # A intersects G
-        # B intersects A
-        # C intersects A
-        # D intersects A
-        # D intersects G
-        # G intersects A
-        # G intersects D
-        result = g.query(
-            """PREFIX geo: <http://www.opengis.net/ont/geosparql#>
-        PREFIX dggs: <https://placeholder.com/dggsfuncs/>
-        SELECT ?a ?b {?a geo:hasGeometry ?a_geom .
-                    ?b geo:hasGeometry ?b_geom .
-                    FILTER dggs:sfIntersects(?a_geom, ?b_geom)
-                    FILTER (?a!=?b)
-                    }"""
-        )
-        result = [{str(k): v for k, v in i.items()} for i in result.bindings]
-        assert len(result) == 10
-        assert {"a": geom_a, "b": geom_b} in result
-        assert {"a": geom_a, "b": geom_c} in result
-        assert {"a": geom_a, "b": geom_d} in result
-        assert {"a": geom_a, "b": geom_g} in result
-        assert {"a": geom_b, "b": geom_a} in result
-        assert {"a": geom_c, "b": geom_a} in result
-        assert {"a": geom_d, "b": geom_a} in result
-        assert {"a": geom_d, "b": geom_g} in result
-        assert {"a": geom_g, "b": geom_a} in result
-        assert {"a": geom_g, "b": geom_d} in result
+def test_intersects():
+    # A intersects B
+    # A intersects C
+    # A intersects D
+    # A intersects G
+    # B intersects A
+    # C intersects A
+    # D intersects A
+    # D intersects G
+    # G intersects A
+    # G intersects D
+    result = g.query(
+        """PREFIX geo: <http://www.opengis.net/ont/geosparql#>
+    PREFIX dggs: <https://placeholder.com/dggsfuncs/>
+    SELECT ?a ?b {?a geo:hasGeometry ?a_geom .
+                ?b geo:hasGeometry ?b_geom .
+                FILTER dggs:sfIntersects(?a_geom, ?b_geom)
+                FILTER (?a!=?b)
+                }"""
+    )
+    result = [{str(k): v for k, v in i.items()} for i in result.bindings]
+    assert len(result) == 10
+    assert {"a": geom_a, "b": geom_b} in result
+    assert {"a": geom_a, "b": geom_c} in result
+    assert {"a": geom_a, "b": geom_d} in result
+    assert {"a": geom_a, "b": geom_g} in result
+    assert {"a": geom_b, "b": geom_a} in result
+    assert {"a": geom_c, "b": geom_a} in result
+    assert {"a": geom_d, "b": geom_a} in result
+    assert {"a": geom_d, "b": geom_g} in result
+    assert {"a": geom_g, "b": geom_a} in result
+    assert {"a": geom_g, "b": geom_d} in result
 
-    def test_touches(self):
-        # A touches C
-        # C touches A
-        result = g.query(
-            """PREFIX geo: <http://www.opengis.net/ont/geosparql#>
-        PREFIX dggs: <https://placeholder.com/dggsfuncs/>
-        SELECT ?a ?b {?a geo:hasGeometry ?a_geom .
-                    ?b geo:hasGeometry ?b_geom .
-                    FILTER dggs:sfTouches(?a_geom, ?b_geom)
-                    }"""
-        )
-        result = [{str(k): v for k, v in i.items()} for i in result.bindings]
-        assert len(result) == 2
-        assert {"a": geom_a, "b": geom_c} in result
-        assert {"a": geom_c, "b": geom_a} in result
+def test_touches():
+    # A touches C
+    # C touches A
+    result = g.query(
+        """PREFIX geo: <http://www.opengis.net/ont/geosparql#>
+    PREFIX dggs: <https://placeholder.com/dggsfuncs/>
+    SELECT ?a ?b {?a geo:hasGeometry ?a_geom .
+                ?b geo:hasGeometry ?b_geom .
+                FILTER dggs:sfTouches(?a_geom, ?b_geom)
+                }"""
+    )
+    result = [{str(k): v for k, v in i.items()} for i in result.bindings]
+    assert len(result) == 2
+    assert {"a": geom_a, "b": geom_c} in result
+    assert {"a": geom_c, "b": geom_a} in result
 
-    def test_equals(self):
-        # D equals G
-        # G equals D
-        result = g.query(
-            """PREFIX geo: <http://www.opengis.net/ont/geosparql#>
-        PREFIX dggs: <https://placeholder.com/dggsfuncs/>
-        SELECT ?a ?b {?a geo:hasGeometry ?a_geom .
-                    ?b geo:hasGeometry ?b_geom .
-                    FILTER dggs:sfEquals(?a_geom, ?b_geom)
-                    FILTER(?a!=?b)
-                    }"""
-        )
-        result = [{str(k): v for k, v in i.items()} for i in result.bindings]
-        assert len(result) == 2
-        assert {"a": geom_d, "b": geom_g} in result
-        assert {"a": geom_g, "b": geom_d} in result
+def test_equals():
+    # D equals G
+    # G equals D
+    result = g.query(
+        """PREFIX geo: <http://www.opengis.net/ont/geosparql#>
+    PREFIX dggs: <https://placeholder.com/dggsfuncs/>
+    SELECT ?a ?b {?a geo:hasGeometry ?a_geom .
+                ?b geo:hasGeometry ?b_geom .
+                FILTER dggs:sfEquals(?a_geom, ?b_geom)
+                FILTER(?a!=?b)
+                }"""
+    )
+    result = [{str(k): v for k, v in i.items()} for i in result.bindings]
+    assert len(result) == 2
+    assert {"a": geom_d, "b": geom_g} in result
+    assert {"a": geom_g, "b": geom_d} in result
 
-    def test_overlaps(self):
-        # D overlaps A
-        # A overlaps D
-        # G overlaps A
-        # A overlaps G
-        result = g.query(
-            """PREFIX geo: <http://www.opengis.net/ont/geosparql#>
-        PREFIX dggs: <https://placeholder.com/dggsfuncs/>
-        SELECT ?a ?b {?a geo:hasGeometry ?a_geom .
-                    ?b geo:hasGeometry ?b_geom .
-                    FILTER dggs:sfOverlaps(?a_geom, ?b_geom) }"""
-        )
-        result = [{str(k): v for k, v in i.items()} for i in result.bindings]
-        assert len(result) == 4
-        assert {"a": geom_a, "b": geom_d} in result
-        assert {"a": geom_d, "b": geom_a} in result
-        assert {"a": geom_a, "b": geom_g} in result
-        assert {"a": geom_g, "b": geom_a} in result
+def test_overlaps():
+    # D overlaps A
+    # A overlaps D
+    # G overlaps A
+    # A overlaps G
+    result = g.query(
+        """PREFIX geo: <http://www.opengis.net/ont/geosparql#>
+    PREFIX dggs: <https://placeholder.com/dggsfuncs/>
+    SELECT ?a ?b {?a geo:hasGeometry ?a_geom .
+                ?b geo:hasGeometry ?b_geom .
+                FILTER dggs:sfOverlaps(?a_geom, ?b_geom) }"""
+    )
+    result = [{str(k): v for k, v in i.items()} for i in result.bindings]
+    assert len(result) == 4
+    assert {"a": geom_a, "b": geom_d} in result
+    assert {"a": geom_d, "b": geom_a} in result
+    assert {"a": geom_a, "b": geom_g} in result
+    assert {"a": geom_g, "b": geom_a} in result
 
-    def test_disjoint(self):
-        # B disjoint C
-        # C disjoint B
-        # B disjoint D
-        # D disjoint B
-        # B disjoint G
-        # G disjoint B
-        # D disjoint C
-        # C disjoint D
-        # G disjoint C
-        # C disjoint G
-        result = g.query(
-            """PREFIX geo: <http://www.opengis.net/ont/geosparql#>
-        PREFIX dggs: <https://placeholder.com/dggsfuncs/>
-        SELECT ?a ?b {?a geo:hasGeometry ?a_geom .
-                    ?b geo:hasGeometry ?b_geom .
-                    FILTER dggs:sfDisjoint(?a_geom, ?b_geom) }"""
-        )
-        result = [{str(k): v for k, v in i.items()} for i in result.bindings]
-        assert len(result) == 10
-        assert {"a": geom_b, "b": geom_c} in result
-        assert {"a": geom_c, "b": geom_b} in result
-        assert {"a": geom_b, "b": geom_d} in result
-        assert {"a": geom_d, "b": geom_b} in result
-        assert {"a": geom_b, "b": geom_g} in result
-        assert {"a": geom_g, "b": geom_b} in result
-        assert {"a": geom_d, "b": geom_c} in result
-        assert {"a": geom_c, "b": geom_d} in result
-        assert {"a": geom_g, "b": geom_c} in result
-        assert {"a": geom_c, "b": geom_g} in result
-
-
-if __name__ == "__main__":
-    unittest.main()
+def test_disjoint():
+    # B disjoint C
+    # C disjoint B
+    # B disjoint D
+    # D disjoint B
+    # B disjoint G
+    # G disjoint B
+    # D disjoint C
+    # C disjoint D
+    # G disjoint C
+    # C disjoint G
+    result = g.query(
+        """PREFIX geo: <http://www.opengis.net/ont/geosparql#>
+    PREFIX dggs: <https://placeholder.com/dggsfuncs/>
+    SELECT ?a ?b {?a geo:hasGeometry ?a_geom .
+                ?b geo:hasGeometry ?b_geom .
+                FILTER dggs:sfDisjoint(?a_geom, ?b_geom) }"""
+    )
+    result = [{str(k): v for k, v in i.items()} for i in result.bindings]
+    assert len(result) == 10
+    assert {"a": geom_b, "b": geom_c} in result
+    assert {"a": geom_c, "b": geom_b} in result
+    assert {"a": geom_b, "b": geom_d} in result
+    assert {"a": geom_d, "b": geom_b} in result
+    assert {"a": geom_b, "b": geom_g} in result
+    assert {"a": geom_g, "b": geom_b} in result
+    assert {"a": geom_d, "b": geom_c} in result
+    assert {"a": geom_c, "b": geom_d} in result
+    assert {"a": geom_g, "b": geom_c} in result
+    assert {"a": geom_c, "b": geom_g} in result
