ccd654f0f1ceb1a88df53762ed52322128468aaa
david-habgood <david.habgood@surroundaustralia.com>
2021-09-30 11:22:08 +1000

    v0.2 - v0.1 had already been used although not released

--- gsdggs/__init__.py
+++ gsdggs/__init__.py
@@ -10,7 +10,7 @@ from .sf_functions import (
 from rdflib import Namespace
 from rdflib.plugins.sparql.operators import register_custom_function
 
-__version__ = "0.1"
+__version__ = "0.2"
 DGGS = Namespace("https://placeholder.com/dggsfuncs/")
 
 register_custom_function(DGGS.sfContains, contains)
